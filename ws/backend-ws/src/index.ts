ed20274aa7081d07fbee209aafc4d0f2b73f77ed
Siddhant Gawad <siddhantgawad4@gmail.com>
2025-09-20 17:39:52 -0400

    made 2 sockets send and recive each other messages - so now will continue on it

--- ws/backend-ws/src/index.ts
+++ ws/backend-ws/src/index.ts
@@ -1,16 +1,39 @@
-import {WebSocketServer} from "ws";
+import { WebSocketServer,WebSocket } from "ws";
 
-const wss = new WebSocketServer({port:3000});
 
-// event handler
-wss.on("connection",function(socket){
-    console.log("User connected");
+//create a websocketserver 
+//intialised web socket server using non native web socket library Websocketserver which we are using because we installed ws 
+const wss = new WebSocketServer({port:3003});
 
-    //client handler
-    socket.on("message",(e)=>{
-        const msg = e.toString();
-            socket.send(msg);
-        
-    })
+
+let userCount = 0;
+// defined a global socket array using native Websoocket 
+let allSockets: WebSocket[] = [];
+
+//whenever there is a new connection to the websocket sewrver call a function and give it the socket 
+wss.on("connection",(socket)=>{
+//wrote this to push messages on one socket to all other sockets so users connected to other sockets can also recieve this message     
+    allSockets.push(socket);
+
+//socket lets u talk to person who just connected to this socket, and can be used to send messages or recieve messages
+//socket servers have no methods, no query paramters 
+userCount=userCount+1;
+console.log("User connected # "+userCount);
+
+//here server uses socket to receive message
+socket.on("message",(message)=>{
+    console.log("Message Recvd: "+message.toString());
+    // now we send the message to all sockets
+    for (let i=0;i<allSockets.length;i++){
+        const s = allSockets[i];
+        s?.send(message.toString() +": sent from the server");
+    }
+
+});
+
+socket.on("disconnect",()=>{
+    allSockets = allSockets.filter(x=>x!=socket);
+})
+
 
 })
